26f5208ccc8056c6dff0f2ee650fe1a0a8a8bbfe
Luis Granda <lgrandab@gmail.com>
2023-03-07 11:54:43 -0500

    Created a GUI for my chatgpt script

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,21 @@
 import openai
 
-openai.api_key = "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"
+import os
+from dotenv import load_dotenv
 
-while True: 
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+while True:
     prompt = input("You: ")
 
     if prompt == "exit":
         break
-    completion = openai.Completion.create(engine="text-davinci-003", 
-                         prompt=prompt,
-                         max_tokens=2048)
+    completion = openai.Completion.create(engine="text-davinci-003",
+                                          prompt=prompt,
+                                          max_tokens=4000)
     print("ChatGPT: " + completion.choices[0].text)
 
- 
+
 print(completion.choices[0].text)
