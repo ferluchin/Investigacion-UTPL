6f6334dc64b1c1e2dfeddce341d59c327ea15d84
ferluchin <lgrandab@gmail.com>
2023-04-17 10:03:12 -0500

    displaying the journals.js graphic

--- javascript/journals.js
+++ javascript/journals.js
@@ -1,21 +1,18 @@
-//import Chart from 'chart.js/auto';
-// import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js';
-//import chartjs from 'https://cdn.skypack.dev/chartjs';
-//import { Chart } from "./charts.js";
-//import { Chart } from 'https://cdn.skypack.dev/chart.js/dist/chart.min.js';
-//import { Chart } from 'https://cdn.skypack.dev/chart.js';
-//import Chart from 'chart.js/auto';
-import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js';
-import Chart from 'chart.js/auto';
+import { Chart, LinearScale, BarController, BarElement, CategoryScale } from 'https://cdn.skypack.dev/chart.js';
+
+Chart.register(LinearScale, BarController, BarElement, CategoryScale);
 
 async function loadCSV() {
     const response = await fetch('./data/new-wos-scopus.csv');
     const csvText = await response.text();
-    return Papa.parse(csvText, { header: true });
+    return Papa.parse(csvText, { header: true, delimiter: ';' }); // Especifica el delimitador aquí
 }
 
 async function showPopularJournals() {
     const { data } = await loadCSV();
+    console.log(data); // Verifica los datos cargados desde el archivo CSV
+
+
     const sourceCounts = {};
 
     for (const row of data) {
@@ -24,12 +21,15 @@ async function showPopularJournals() {
             sourceCounts[source] = (sourceCounts[source] || 0) + 1;
         }
     }
+    console.log(sourceCounts); // Verifica si las revistas y sus recuentos están correctamente acumulados
 
     const sortedSources = Object.entries(sourceCounts).sort((a, b) => b[1] - a[1]);
 
     // Crea una lista de etiquetas y datos para la gráfica
     const labels = sortedSources.map(([source]) => source);
     const dataPoints = sortedSources.map(([_, count]) => count);
+
+    console.log(labels, dataPoints); // Verifica si las etiquetas y los puntos de datos están correctamente formateados
 
     const popularJournalsDiv = document.getElementById('journalsChart');
     const chartCanvas = document.createElement('canvas');
@@ -52,7 +52,11 @@ async function showPopularJournals() {
             responsive: true,
             maintainAspectRatio: false,
             scales: {
+                x: {
+                    type: 'category',
+                },
                 y: {
+                    type: 'linear',
                     beginAtZero: true
                 }
             }
